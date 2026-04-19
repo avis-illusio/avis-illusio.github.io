4934b324c615b7697df08478bfd9e2e9ca178199
avis-illusio <avis-illusio@outlook.com>
2026-04-19 22:36:56 +0800

    2026/4/19 22:36:43

--- git_pull.cmd
+++ git_pull.cmd
@@ -1,6 +1,7 @@
+$timestamp = Get-Date -Format "yyyy/M/d HH:mm:ss"
 pnpm astro build
 git remote add origin "git@github.com:avis-illusio/avis-illusio.github.io.git"
 git add .
-git commit -m "2026/4/19"
+git commit -m "$timestamp"
 git branch -M main
 git push -u origin main --force
