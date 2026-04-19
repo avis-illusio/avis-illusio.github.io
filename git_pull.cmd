6bb724e22d3d6bd92657df582d34b78a3a78fc44
avis-illusio <avis-illusio@outlook.com>
2026-04-19 19:23:53 +0800

    2026/4/19

--- git_pull.cmd
+++ git_pull.cmd
@@ -1,4 +1,5 @@
 git remote add origin "git@github.com:avis-illusio/avis-illusio.github.io.git"
 git add .
 git commit -m "2026/4/19"
-git push -u origin main
+git branch -M main
+git push -u origin main --force
